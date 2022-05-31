321c67c07237e84595d0c7ee4bf0ab8b87d3d6f4
Alapottra <alapottratrojon@gmail.com>
2022-06-01 01:19:13 +0600

    transition and animate on click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {motion} from 'framer-motion'
+import { useState } from 'react';
 
 function App() {
+  const [rotate, setRotate] = useState(false)
+  const rotateAnimation = {
+      rotate: rotate ? 500 : 0,
+ 
+     
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < motion.div  transition={{ ease: "easeOut", duration: 1 }} animate={rotateAnimation} onClick={() => setRotate(!rotate)} className="animate-box">
+
+      </motion.div>
     </div>
   );
 }
