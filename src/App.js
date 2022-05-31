bddbac0a53a35ef5863716e151c6907b3525f491
Alapottra <alapottratrojon@gmail.com>
2022-06-01 01:28:23 +0600

    added move to x axis on click

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import { useState } from 'react';
 
 function App() {
   const [rotate, setRotate] = useState(false)
+  const [move, setMove] = useState(false)
   const rotateAnimation = {
-      rotate: rotate ? 500 : 0,
- 
-     
+    rotate: rotate ? 500 : 0,
+
+
+  }
+  const moveAnimation = {
+    x : move ? 200 : 0,
   }
 
   return (
     <div className="App">
-      < motion.div  transition={{ ease: "easeOut", duration: 1 }} animate={rotateAnimation} onClick={() => setRotate(!rotate)} className="animate-box">
+      < motion.div transition={{ ease: "easeOut", duration: 1 }} animate={rotateAnimation} onClick={() => setRotate(!rotate)} className="animate-box">
+
+      </motion.div>
+      < motion.div  animate={moveAnimation} onClick={() => setMove(!move)} className="animate-box">
 
       </motion.div>
     </div>
