8df83fc81386bf9e6fe149d173b74e41b010621e
Alapottra <alapottratrojon@gmail.com>
2022-06-01 15:54:59 +0600

    added more animations

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   }
   const moveAnimation = {
-    x : move ? 200 : 0,
+    x: move ? 200 : 0,
   }
 
   return (
@@ -19,9 +19,28 @@ function App() {
       < motion.div transition={{ ease: "easeOut", duration: 1 }} animate={rotateAnimation} onClick={() => setRotate(!rotate)} className="animate-box">
 
       </motion.div>
-      < motion.div  animate={moveAnimation} onClick={() => setMove(!move)} className="animate-box">
+      < motion.div animate={moveAnimation} onClick={() => setMove(!move)} className="animate-box">
 
       </motion.div>
+
+      < motion.div whileHover={{scale : 2}} className="animate-box">
+            <h2>Hello World</h2>
+      </motion.div>
+
+      < motion.div drag whileDrag={{scale : 1.2}} className="animate-box">
+            <h2>Drag me</h2>
+      </motion.div>
+
+      {/* squence animation */}
+
+      < motion.div animate={{rotate : [0,200,200,0]}}  className="animate-box">
+          <h2>Squence</h2>
+      </motion.div>
+
+      < motion.div transition={{ ease : 'easeOut' ,repeat : Infinity}} animate={{rotate : [0,200,200,0] , x: [0,100,200,200,100,0]}}  className="animate-box">
+            <h2>Infinite Animation</h2>
+      </motion.div>
+
     </div>
   );
 }
